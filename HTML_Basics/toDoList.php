fa2f9b6492718c57de3752e98c907d9dc594883b
rds493 <rds493@nyu.edu>
2017-07-29 19:48:31 -0400

    Lessons in HTML

---
+++ HTML_Basics/toDoList.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>
+            To Do List - July 16
+        </title>
+    </head>
+    <body>
+        <h1>
+            Ricardo's To Do List 
+        </h1>
+        
+        <h2>
+            July 15<sup>th</sup>
+        </h2>
+        
+        <ul>
+            <li>
+                Read at least 30 pages of HTML book
+            </li>
+            <li>
+                Help preparing dinner
+            </li>
+            <li>
+                Apologize to Ashley
+            </li>
+        </ul>
+        <hr />
+        
+        <p>Now Ordered</p>
+        
+        <ol>
+            <li>
+                Read at least 30 pages of HTML book
+            </li>
+            <li>
+                Help preparing dinner
+            </li>
+            <li>
+                Apologize to Ashley
+            </li>
+            <li>
+                Check ID Game Plan
+                <ul>
+                    <li>
+                        Prepare for Cryptography and Cybersecurity
+                    </li>
+                </ul>
+            </li>
+        </ol>
+    </body>
+</html>
