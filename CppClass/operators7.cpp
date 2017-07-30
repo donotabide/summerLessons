31a2020a621019bc8c75268e374da04b4869fc1d
rds493 <rds493@nyu.edu>
2017-07-30 17:13:04 -0400

    Operators Example

---
+++ CppClass/operators7.cpp
@@ -0,0 +1,25 @@
+#include "headerExample5.h"
+
+int main(){
+
+  int exampleInc = 10;
+
+  cout << "\n\nexampleInc++ Before: "<< exampleInc++ << endl;
+  cout << "exampleInc++ After: "<< exampleInc << endl;
+
+  cout << "++exampleInc Before: "<< ++exampleInc << endl;
+  cout << "++exampleInc After: "<< exampleInc << endl;
+
+
+  int exampleDec = 10;
+
+  cout << "\n\nexampleDec-- Before: "<< exampleDec-- << endl;
+  cout << "exampleDec-- After: "<< exampleDec << endl;
+
+  cout << "--exampleInc Before: "<< --exampleDec << endl;
+  cout << "--exampleInc After: "<< exampleDec << "\n\n";
+
+
+  int exampleTimes = 10;
+
+}
