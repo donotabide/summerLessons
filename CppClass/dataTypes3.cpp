62c70888bc7a4a5f983a78a39a5f4b3a2ffa2fc8
rds493 <rds493@nyu.edu>
2017-07-30 09:06:01 -0400

    C++ Class Intro to Data Types

---
+++ CppClass/dataTypes3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+  int year = 1988;
+  double pi = 3.14159265;
+  float heightInMeter = 1.92f;
+
+  cout << "I was born in the year " << year << endl;
+  cout << "So by the end this year I will be " << 2017 - year << endl;
+  cout << "I am "<< heightInMeter<<" meters tall." << endl;
+  cout << "Some of the digits of Pi are: " << pi << "."<< endl;
+
+}
---
+++ CppClass/variables2.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  string sayWhaat = "What whaaaaaaaaat?";
+
+  cout << "What do you say? "<< endl;
+  cout << sayWhaat << endl;
+
+  return(0);
+}
