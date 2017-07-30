ab8fe32d4020462e673598fcf5bbc50c349fe497
rds493 <rds493@nyu.edu>
2017-07-30 12:27:11 -0400

    Example for user Input

---
+++ CppClass/userInput4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    // Declaring string variables to store user input
+    string name;
+    string birthday;
+
+    cout << "\n\n\n";
+
+    cout << "What is your name? " << endl;
+    cin >> name;
+
+    cout << "When were you born? " << endl;
+    cin >> birthday;
+
+
+    cout << "My name is: " << name << endl;
+    cout << "I was born in: " << birthday << endl;
+
+    cout << "\n\n\n";
+
+
+}
